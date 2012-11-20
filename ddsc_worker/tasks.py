b3b216a95c4254302776fdbb67bab948ba12d3d3
byrman <carsten.byrman@nelen-schuurmans.nl>
2012-11-20 14:18:07 +0100

    Add sleep to simulate work

--- ddsc_worker/tasks.py
+++ ddsc_worker/tasks.py
@@ -1,12 +1,15 @@
 from __future__ import absolute_import
 from ddsc_worker.celery import celery
+import time
 
 
 @celery.task
 def add(x, y):
+    time.sleep(10)
     return x + y
 
 
 @celery.task
 def mul(x, y):
-    return x + y
+    time.sleep(2)
+    return x * y
