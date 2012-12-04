90fd6ca6b210fabc01a548f37be996b11047dd67
byrman <carsten.byrman@nelen-schuurmans.nl>
2012-12-04 10:46:57 +0100

    Change GPL into MIT

--- ddsc_worker/admin.py
+++ ddsc_worker/admin.py
@@ -1,9 +1,6 @@
-# (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.rst.
+# (c) Nelen & Schuurmans. MIT licensed, see LICENSE.rst.
 from __future__ import unicode_literals
-
+from ddsc_worker import models
 from django.contrib import admin
 
-from ddsc_worker import models
-
-
-# admin.site.register(models.TODO)
+#admin.site.register(models.TODO)
--- ddsc_worker/views.py
+++ ddsc_worker/views.py
@@ -1,7 +1,7 @@
-# (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.rst.
+# (c) Nelen & Schuurmans. MIT licensed, see LICENSE.rst.
 from __future__ import unicode_literals
 
-from django.utils.translation import ugettext as _
+# from django.utils.translation import ugettext as _
 # from django.core.urlresolvers import reverse
 # from lizard_map.views import MapView
 # from lizard_ui.views import UiView
