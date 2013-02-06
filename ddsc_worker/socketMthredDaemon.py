fcb8b9d1aac9df42c12ad1e7e9f97cf3156b7478
shaoqing <shaoqing@ldam-fgslinux.(none)>
2013-02-06 09:08:34 +0100

    checked for pep8

--- ddsc_worker/socketMthredDaemon.py
+++ ddsc_worker/socketMthredDaemon.py
@@ -3,7 +3,7 @@ Created on 14 nov. 2012
 
 @author: LuS
 
-Based on Python offical document 
+Based on Python offical document
 20.17.4.3. Asynchronous Mixins
 '''
 import threading
@@ -18,51 +18,51 @@ from django.conf import settings
 
 DST_PATHS = getattr(settings, 'PATH_DST')
 
-class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         print "connection established with:  %r" % (self.client_address[0])
         first_time = time.time()
         current_time = time.time()
         timeout = 20
-        
-        path = DST_PATHS['socket']   ## TO BE put in a django setting file
-        fileName = self.client_address[0] + '_' + str(self.client_address[1]) + '_'
-        
-        i=1
-        while 1:           
-            f = open(path+fileName+str(i)+'.csv', 'wb')
+        path = DST_PATHS['socket']   # TO BE put in a django setting file
+        fileName = self.client_address[0] + '_'
+        + str(self.client_address[1]) + '_'
+        i = 1
+        while 1:
+            f = open(path + fileName + str(i) + '.csv', 'wb')
             while timeout > (current_time - first_time):
-                try :
+                try:
                     self.request.send("ok")
                     data = self.request.recv(1024)
-                    f.write(data+'\r\n')
-                    print "%r:%r wrote a line in %r" % (self.client_address[0],
-                                                        self.client_address[1],f)
+                    f.write(data + '\r\n')
+                    print "%r:%r wrote a line in %r" % (
+                    self.client_address[0],
+                    self.client_address[1], f)
                     current_time = time.time()
-                except :
-                    print "connection with %r:%r lost" % (self.client_address[0], 
-                                                          str(self.client_address[1]))
+                except:
+                    print "connection with %r:%r lost" % (
+                    self.client_address[0],
+                    str(self.client_address[1]))
                     exit()
-            i+=1
+            i += 1
             first_time = time.time()
 
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
+
 class App():
-   
     def __init__(self):
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/tty'
         self.stderr_path = '/dev/tty'
-        self.pidfile_path =  '/var/run/ddsc/tcpMthredDaemon.pid'
+        self.pidfile_path = '/var/run/ddsc/tcpMthredDaemon.pid'
         self.pidfile_timeout = 5
-           
+
     def run(self):
         #Main code goes here ...
-        #Note that logger level needs to be set to logging.DEBUG before this shows up in the logs
         logger.debug("Debug message")
         logger.info("Info message")
         logger.warn("Warning message")
@@ -70,7 +70,6 @@ class App():
 
         HOST, PORT = "10.10.101.118", 5008
         server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-        ip, port = server.server_address
 
         server_thread = threading.Thread(target=server.serve_forever)
     # Exit the server thread when the main thread terminates
@@ -83,12 +82,12 @@ class App():
 app = App()
 logger = logging.getLogger("DaemonLog")
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler = logging.FileHandler("/var/log/ddsc/testdaemon.log")  ## TO be put in a django setting file
+formatter = logging.Formatter
+("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler = logging.FileHandler("/var/log/ddsc/testdaemon.log")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 daemon_runner = runner.DaemonRunner(app)
-#This ensures that the logger file handle does not get closed during daemonization
-daemon_runner.daemon_context.files_preserve=[handler.stream]
+daemon_runner.daemon_context.files_preserve = [handler.stream]
 daemon_runner.do_action()
