655635391d10e12681951a9f5c4dcd419866c00c
byrman <carsten.byrman@nelen-schuurmans.nl>
2013-02-26 12:24:10 +0100

    Simplify Celery configuration

--- ddsc_worker/admin.py
+++ ddsc_worker/admin.py
@@ -1,6 +1,9 @@
 # (c) Nelen & Schuurmans. MIT licensed, see LICENSE.rst.
-from __future__ import unicode_literals
-from ddsc_worker import models
-from django.contrib import admin
 
-#admin.site.register(models.TODO)
+from __future__ import unicode_literals
+
+##from django.contrib import admin
+
+##from ddsc_worker import models
+
+##admin.site.register(models.TODO)
--- ddsc_worker/celeryconfig.development.py
+++
@@ -1,6 +0,0 @@
-"""Celery development settings"""
-
-BROKER_URL = "amqp://guest@rabbitmq:5672//"
-CELERY_IMPORTS = ("ddsc_worker.tasks", )
-
-CELERY_TASK_SERIALIZER = "json"
