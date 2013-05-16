b939d27df48aea0f824c61b9c8007df606ecc1d4
shaoqing <s.lu@fugro.nl>
2013-05-16 11:05:56 +0200

    no longer needed

--- ddsc_worker/management/commands/timeseries_dataset_map.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from ddsc_core.models import Timeseries
-
-
-#class Command(BaseCommand):
-#    def handle(self):
-for ts in Timeseries.objects.all():
-    ts.data_set = [1]
